c0b55b636c425f0fd0c0625ccfb440d86d88bf4e
RITAM DE <ritamdey.1998@gmail.com>
2023-10-13 23:09:37 +0530

    add all files in git hub

---
+++ src/main/java/com/example/devopsintegration/DevOpsIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.example.devopsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsIntegrationApplication.class, args);
+	}
+}
